2fd92dc78c559979e0deb45df3b538d91c655336
Srivathsan <srivathsan.mit@gmail.com>
2024-01-21 18:04:15 +0530

    Security enhancements

--- src/main/java/com/srilearning/BookMyShow/Controllers/UserController.java
+++ src/main/java/com/srilearning/BookMyShow/Controllers/UserController.java
@@ -3,7 +3,6 @@ import com.srilearning.BookMyShow.Exceptions.UserNotAvailableException;
 import com.srilearning.BookMyShow.Models.User;
 import com.srilearning.BookMyShow.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
